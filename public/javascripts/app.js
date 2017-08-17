a5a4062ddf1b756205764d8eaf6f83c6eada46bb
Yogesh Gupta <yogesh201996@gmail.com>
2017-08-17 23:06:25 +0530

    Map scripts

---
+++ public/javascripts/app.js
@@ -0,0 +1,30 @@
+function initMap(pos) {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    console.log(startPos);
+    initMap(pos);
+  };
+  var geoError = function(error) {
+    console.log('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from location provider)
+    //   3: timed out
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}; 
